ed4c6935590b921ae9e13febc80ed74c56650a54
Mike Codeur <contact@mikecodeur.com>
2024-10-11 16:19:18 +0800

    export

--- src/app/exercises/dynamic-rendering/cache-options/page.tsx
+++ src/app/exercises/dynamic-rendering/cache-options/page.tsx
@@ -1,6 +1,8 @@
 //export {default} from './page.final'
 
 export {default} from './page.exercise'
+// eslint-disable-next-line import/export
+export * from './page.exercise'
 
 //1. 🚀 force-dynamique
 // export {default} from './page.bonus-1'
