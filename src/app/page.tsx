86fc0dfc098ec363d6a03c1a4e1b79f3e1bc5373
Mike Codeur <contact@mikecodeur.com>
2024-10-30 20:25:22 +0800

    update next 15

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,22 +77,6 @@ export default function Home() {
             Accès aux exercices.
           </p>
         </Link>
-
-        <Link
-          href="/exercises/static-rendering"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            static-rendering
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Affichage des `posts` statiquement.
-          </p>
-        </Link>
       </div>
     </main>
   )
