83a53eaa03daaacdb6bf93da0dc3b65606ea3a92
tloyan <thomas.loyan@gmail.com>
2024-11-21 00:17:17 +0100

    feature/composition-patterns

--- src/app/exercises/composition/page.exercise.tsx
+++ src/app/exercises/composition/page.exercise.tsx
@@ -1,35 +1,34 @@
 import RenderTime from '@/components/render-time'
-// 🐶 Importe les composants Server et Client
-// import Server from './server'
-// import Client from './client'
 
-// 🐶 Importe les composants `ServerWithClient` (qui inclus un client)
-// et `ClientWithServer` (qui inclus un serveur)
-// import {ClientWithServer} from './clientWithServer'
-// import {ServerWithClient} from './serverWithClient'
+import Server from './server'
+import Client from './client'
+
+import {ClientWithServer} from './clientWithServer'
+import {ServerWithClient} from './serverWithClient'
 
 const Page = async () => {
   return (
     <div className="mx-auto flex max-w-4xl flex-col gap-1 p-6 text-lg">
       <h1 className="mb-4 text-center text-3xl font-bold"> Composition</h1>
       <div className="mt-2 grid grid-cols-2 gap-2">
-        {/* 🐶 Fais la composition suivante (props children) */}
-        {/* 
         <Client>
           <Server>
             <Client>
               <Server />
             </Client>
           </Server>
-        </Client> 
-        */}
+        </Client>
 
-        {/* 🐶 Fais la composition inverse (props children) */}
-        {/* Server -> Client -> Server -> Client */}
+        <Server>
+          <Client>
+            <Server>
+              <Client />
+            </Server>
+          </Client>
+        </Server>
 
-        {/* 🐶 Utilise <ServerWithClient /> */}
-
-        {/* 🐶 Utilise <ClientWithServer /> */}
+        <ServerWithClient />
+        <ClientWithServer />
       </div>
       <RenderTime />
     </div>
--- src/app/exercises/composition/server.exercise.tsx
+++ src/app/exercises/composition/server.exercise.tsx
@@ -1,14 +1,13 @@
-'use server'
 import React from 'react'
 import {CardComponentType} from '@/components/card-component-type'
 import {ComponentTypeEnum, detectActualType} from '@/lib/helper'
 
-export default async function Server({children}: {children?: React.ReactNode}) {
-  await new Promise((resolve) => setTimeout(resolve, 100))
+export default function Server({children}: {children?: React.ReactNode}) {
+  // await new Promise((resolve) => setTimeout(resolve, 100))
   const actualType = detectActualType()
   return (
     <CardComponentType
-      componentType={ComponentTypeEnum.SERVER}
+      componentType={ComponentTypeEnum.ISOMORPHIC}
       actualType={actualType}
     >
       {children}
